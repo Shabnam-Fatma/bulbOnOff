74f1378f6cb4ffc0464653ce20f1366f8dd5c98f
Shabnam-Fatma <shabnamfatma.tech@gmail.com>
2024-12-11 05:09:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let bulbOffPic = document.querySelector(".bulboff");
+
+
+function bulbOn() {
+    bulbOffPic.setAttribute("src", "pic_bulbon.gif");
+}
+
+function bulbOff() {
+    bulbOffPic.setAttribute("src", "pic_bulboff.gif");
+}
+
